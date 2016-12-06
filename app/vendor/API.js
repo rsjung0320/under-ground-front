489c41cf2ecdb06e8365c9909d2794513212e3f7
rsjung0320 <rsjung0320@gmail.com>
2016-12-06 10:50:49 +0900

    1. Artist 화면 변경 2. Google 분석 추가

--- app/vendor/API.js
+++ app/vendor/API.js
@@ -5,17 +5,17 @@ var API = (function() {
   var POST = 'POST';
   var DELETE = 'DELETE';
 
-  var postRefreshToken = '/login/token/refresh/';
-  var postRegenerateToken = '/login/token/regenerate/';
-  var postSignin = '/login/signin/';
-  var postSignUp = '/login/signup/';
-  var postUserInfo = '/api/v1/user/';
+  // var postRefreshToken = '/login/token/refresh/';
+  // var postRegenerateToken = '/login/token/regenerate/';
+  // var postSignin = '/login/signin/';
+  // var postSignUp = '/login/signup/';
+  // var postUserInfo = '/api/v1/user/';
 
-  // var postRefreshToken = 'http://localhost:9999/login/token/refresh/';
-  // var postRegenerateToken = 'http://localhost:9999/login/token/regenerate/';
-  // var postSignin = 'http://localhost:9999/login/signin/';
-  // var postSignUp = 'http://localhost:9999/login/signup/';
-  // var postUserInfo = 'http://localhost:9999/api/v1/user/';
+  var postRefreshToken = 'http://localhost:9999/login/token/refresh/';
+  var postRegenerateToken = 'http://localhost:9999/login/token/regenerate/';
+  var postSignin = 'http://localhost:9999/login/signin/';
+  var postSignUp = 'http://localhost:9999/login/signup/';
+  var postUserInfo = 'http://localhost:9999/api/v1/user/';
 
 
   /*----------- API Interface -----------------*/
